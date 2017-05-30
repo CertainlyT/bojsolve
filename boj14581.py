e185ad6e8b631361937c7fc12bfca6a1959844e4
Jihyeon Jeong <xcm1321@gmail.com>
2017-05-30 18:52:45 +0900

    modify 14581

--- boj14581.py
+++ boj14581.py
@@ -1,14 +1,4 @@
 string = input()
-
-str_list = [":fan:", ":", string, ":", ":fan:"]
-for i in range(3):
-    print(":fan:", end="")
-print()
-
-for k in range(len(str_list)):
-    print(str_list[k], end="")
-print()
-
-for j in range(3):
-    print(":fan:", end="")
-print()
+print(":fan::fan::fan:")
+print(":fan:" + ":" + string + ":" + ":fan:")
+print(":fan::fan::fan:")
