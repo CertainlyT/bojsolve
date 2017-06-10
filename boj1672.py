06eb021745068ca32694fd373553d579cbc90867
CertainlyT <xcm1321@gmail.com>
2017-06-10 11:11:30 +0900

    solve 1672

--- boj1672.py
+++ boj1672.py
@@ -1,7 +1,21 @@
 n = int(input())
-gene = input()
+dna = list(input())
+dna.reverse()
 
-for i in range(len(gene), -1, -1):
-    if gene[i] == gene[i-1]:
+for i in range(n-1):
+    if dna[i] == dna[i+1]:
         pass
-    elif gene[i-1]
+    elif (dna[i] == "A" and dna[i + 1] == "G") or (dna[i] == "G" and dna[i + 1] == "A"):
+        dna[i+1] = "C"
+    elif (dna[i] == "C" and dna[i + 1] == "A") or (dna[i] == "A" and dna[i + 1] == "C"):
+        dna[i + 1] = "A"
+    elif (dna[i] == "A" and dna[i + 1] == "T") or (dna[i] == "T" and dna[i + 1] == "A"):
+        dna[i + 1] = "G"
+    elif (dna[i] == "C" and dna[i + 1] == "G") or (dna[i] == "G" and dna[i + 1] == "C"):
+        dna[i + 1] = "T"
+    elif (dna[i] == "G" and dna[i + 1] == "T") or (dna[i] == "T" and dna[i + 1] == "G"):
+        dna[i + 1] = "A"
+    else:
+        dna[i + 1] = "G"
+
+print(dna[n-1])
