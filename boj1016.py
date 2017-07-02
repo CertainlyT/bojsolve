b5a0a0cb7a6bbf8821891130939dc5f43056a8c5
CertainlyT <xcm1321@gmail.com>
2017-07-02 16:06:33 +0900

    1016: modified but TLE

--- boj1016.py
+++ boj1016.py
@@ -1,15 +1,26 @@
+def getPrimeList(num):
+    global primes
+    primes = []
+
+    if num < 2:
+        return primes
+    for i in range(2, num+1):
+        isPrime = True
+        for j in primes:
+            if i % j == 0:
+                isPrime = False
+                break
+            elif j > i**0.5:
+                break
+        if isPrime:
+            primes.append(i)
+    return primes
+
 a, b = map(int, input().split())
+num = b - a + 1
+getPrimeList(b)
 
-i = 2
-num_list = {4}
-while i**2 < b:
-    num_list.add(i**2)
-    i += 1
+for i in primes:
+    num -= b // i ** 2
 
-for i in sorted(num_list):
-    for j in range(i*2, b + 1, i):
-        if j > b:
-            break
-        num_list.add(j)
-
-print(b - a + 1 - len(num_list))
+print(num)
