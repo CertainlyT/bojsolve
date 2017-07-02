051eb4aef8a47201294a48a0c77251f17c897003
CertainlyT <xcm1321@gmail.com>
2017-07-02 15:42:46 +0900

    solve 11053, 11722

--- boj11722.py
+++ boj11722.py
@@ -8,23 +8,23 @@ for i in range(n):
     if i == 0:
         check_list.append(num_list[i])
     else:
-        if num_list[i] > max(check_list):
+        if num_list[i] < min(check_list):
             check_list.append(num_list[i])
         else:
             len_check = len(check_list)
-            if check_list[len_check - 2] < num_list[i] < check_list[len_check-1]:
+            if check_list[len_check - 2] > num_list[i] > check_list[len_check-1]:
                 check_list[len_check-1] = num_list[i]
             else:
                 if len(check_list) == 1:
-                    if num_list[i] < check_list[0]:
+                    if num_list[i] > check_list[0]:
                         check_list[0] = num_list[i]
                 else:
                     for j in range(len(check_list)-1):
                         if j == 0:
-                            if num_list[i] < check_list[j]:
+                            if num_list[i] > check_list[j]:
                                 check_list[j] = num_list[i]
                         else:
-                            if check_list[j-1] < num_list[i] < check_list[j]:
+                            if check_list[j-1] > num_list[i] > check_list[j]:
                                 check_list[j] = num_list[i]
 
 print(len(check_list))
