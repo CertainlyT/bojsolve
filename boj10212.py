e669f03798bddc89d30768a51b6954d17bb97918
CertainlyT <xcm1321@gmail.com>
2017-07-11 21:21:28 +0900

    solve 10212, 11943

---
+++ boj10212.py
@@ -0,0 +1,2 @@
+from random import randint
+print(["Korea", "Yonsei"][randint(0, 1)])
---
+++ boj11943.py
@@ -0,0 +1,5 @@
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+
+lst = [a + d, b + c]
+print(min(lst))
