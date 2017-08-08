7bdbb197dcec88ef69af6e0992e7fb6fe85dd4d2
CertainlyT <xcm1321@gmail.com>
2017-08-08 21:24:10 +0900

    modify 11721: simplified source

--- boj11721.py
+++ boj11721.py
@@ -1,16 +1,7 @@
 string = input()
-n = len(string)
-a = n // 10
-b = n % 10
-if a == 0:
-    for i in range(b):
-        print(string[i], end="")
-else:
-    for i in range(1, a+1):
-        for j in range(10*(i-1), 10*i):
-            print(string[j], end="")
-        print()
-    else:
-        for i in range(10*a, 10*a+b):
-            print(string[i], end="")
-        print()
+
+for i in range(0, len(string), 10):
+    try:
+        print(string[i:i+10])
+    except IndexError:
+        print(string[i:len(string)])
