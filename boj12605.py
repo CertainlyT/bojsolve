c558126c449d7e7b2969c200ed67683aef903f23
CertainlyT <xcm1321@gmail.com>
2017-10-21 14:44:00 +0900

    12605: short coding

--- boj12605.py
+++ boj12605.py
@@ -1,6 +1,5 @@
 for i in range(int(input())):
-    lst = list(map(str, input().split()))
     print("Case #%d:" % (i + 1), end=" ")
-    for j in range(len(lst) - 1, -1, -1):
-        print(lst[j], end=" ")
+    for j in input().split()[::-1]:
+        print(j, end=" ")
     print()
