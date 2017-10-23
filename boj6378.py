ff0ba207ad4b2510696aaf7c148602ecb12d9bd5
CertainlyT <xcm1321@gmail.com>
2017-10-23 11:13:35 +0900

    6378: solve without list

--- boj6378.py
+++ boj6378.py
@@ -1,27 +1,13 @@
-str_list = []
-num_list = []
-less_num = []
-while True:
-    n = int(input())
-    if n == 0:
+while 1:
+    n = input()
+    if n == "0":
         break
-    num_list.append(n)
-
-for i in range(len(num_list)):
-    a = str(num_list[i])
-    total_num = 0
-    for j in range(len(a)):
-        total_num += int(a[j])
-        if total_num < 10:
-            continue
-        else:
-            t = str(total_num)
-            total_num = 0
-            for o in range(len(t)):
-                total_num += int(t[o])
-    str_list.append(total_num)
-
-
-for k in range(len(str_list)):
-    print(str_list[k])
-
+    result = 0
+    for i in range(len(n)):
+        result += int(n[i])
+    while result >= 10:
+        temp = 0
+        for i in range(len(str(result))):
+            temp += int(str(result)[i])
+        result = temp
+    print(result)
