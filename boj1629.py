f3bf5336e99f65d1b5b6cf93ea369f35b3e7e39e
CertainlyT <xcm1321@gmail.com>
2017-10-30 14:06:26 +0900

    solve 1629

--- boj1629.py
+++ boj1629.py
@@ -1,12 +1,14 @@
-def f(x, n):
+def f(x, n, mod):
     if n == 0:
         return 1
     elif n % 2 == 1:
-        return x * f(x, n-1)
+        half = f(x, n // 2, mod)
+        return x * half * half % mod
     else:
-        half = f(x, n//2)
-        return half * half
+        half = f(x, n//2, mod)
+        return half * half % mod
+
 
 a, b, c = map(int, input().split())
 
-print(f(a, b) % c)
+print(f(a, b, c))
