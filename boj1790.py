0e44016f42731cf4bf40e47862b7fa4072da002d
CertainlyT <xcm1321@gmail.com>
2017-11-26 12:29:47 +0900

    try 1790: not yet

--- boj1790.py
+++ boj1790.py
@@ -1,27 +1,14 @@
 n, k = map(int, input().split())
 
-result = 0
-length = len(str(n))
+nine = 9
+length = 1
+ten = 1
 
-while n != 0:
-    temp = "1" + "0" * (len(str(n)) - 1)
-    temp2 = n - int(temp) + 1
-    result += temp2 * length
-    n = n - temp2
-    length -= 1
+while n - nine * ten >= 0:
+    n -= nine * ten
+    ten *= 10
+    k -= length * nine * ten
+    length += 1
 
-case = 9
-num = 1
-while result - (case * num * 1) > 0:
-    result -= case * num
-    num *= 10
-    k -= case
-
-a = k // len(str(num))
-
-test_case = ""
-for i in range(a):
-    test_case += str(num)
-    num += 1
-
-print(test_case[k-1])
+print(n, k)
+print(length)
